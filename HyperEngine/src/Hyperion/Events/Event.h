9c689134c496bdb6d3529e78e2c8728a05f684e0
SkillerRaptor <skillerraptor@protonmail.com>
2020-08-29 20:58:43 +0200

    Fixing Namespace

--- HyperEngine/src/Hyperion/Events/Event.h
+++ HyperEngine/src/Hyperion/Events/Event.h
@@ -2,23 +2,25 @@
 
 #include <iostream>
 
-enum class EventType : uint8_t
+namespace Hyperion
 {
-	None = 0,
-	WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
-	MouseMoved, MouseScrolled, MouseButtonPressed, MouseButtonReleased,
-	KeyPressed, KeyReleased, KeyTyped
-};
+	enum class EventType : uint8_t
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		MouseMoved, MouseScrolled, MouseButtonPressed, MouseButtonReleased,
+		KeyPressed, KeyReleased, KeyTyped
+	};
 
-enum EventCategory : uint8_t
-{
-	NoneCat = 0,
-	WindowCategory,
-	MouseCategory,
-	KeyCategory,
-	InputCategory,
-	CustomCategory
-};
+	enum EventCategory : uint8_t
+	{
+		NoneCat = 0,
+		WindowCategory,
+		MouseCategory,
+		KeyCategory,
+		InputCategory,
+		CustomCategory
+	};
 
 #define EVENT_CLASS_TYPE(type) static EventType GetStaticEventType() { return EventType::type; }\
 								virtual EventType GetEventType() const override { return GetStaticEventType(); }\
@@ -26,48 +28,49 @@ enum EventCategory : uint8_t
 
 #define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
 
-class Event
-{
-public:
-	virtual ~Event() = default;
-
-	bool Handled = false;
-
-	virtual EventType GetEventType() const = 0;
-	virtual const char* GetName() const = 0;
-	virtual int GetCategoryFlags() const = 0;
-	virtual std::string ToString() const { return GetName(); }
-
-	bool IsInCategory(EventCategory category)
+	class Event
 	{
-		return GetCategoryFlags() & category;
-	}
-};
+	public:
+		virtual ~Event() = default;
 
-class EventDispatcher
-{
-private:
-	Event& m_Event;
+		bool Handled = false;
 
-public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
 
-	EventDispatcher(Event& event)
-		: m_Event(event)
-	{}
-
-	template<typename T, typename F>
-	bool Dispatch(const F& function)
-	{
-		if (m_Event.GetEventType() == T::GetStaticEventType())
+		bool IsInCategory(EventCategory category)
 		{
-			m_Event.Handled = function(static_cast<T&>(m_Event));
-			return true;
+			return GetCategoryFlags() & category;
 		}
-		return false;
-	}
-};
+	};
 
-inline std::ostream& operator<<(std::ostream& os, const Event& event)
-{
-	return os << event.ToString();
+	class EventDispatcher
+	{
+	private:
+		Event& m_Event;
+
+	public:
+
+		EventDispatcher(Event& event)
+			: m_Event(event)
+		{}
+
+		template<typename T, typename F>
+		bool Dispatch(const F& function)
+		{
+			if (m_Event.GetEventType() == T::GetStaticEventType())
+			{
+				m_Event.Handled = function(static_cast<T&>(m_Event));
+				return true;
+			}
+			return false;
+		}
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& event)
+	{
+		return os << event.ToString();
+	}
 }
