193df2c9f331084ef7b3e576c98bc17369bb9315
SkillerRaptor <skillerraptor@protonmail.com>
2020-08-29 20:12:20 +0200

    Adding Client Includes

--- HyperEngine/src/Hyperion.h
+++ HyperEngine/src/Hyperion.h
@@ -1,3 +1,16 @@
 #pragma once
 
 /* Include File for HyperEngine Application / Projects */
+
+/* Memory */
+#include "Hyperion/Memory/PoolAllocator.h"
+
+/* Rendering */
+#include "Hyperion/Rendering/Shader.h"
+#include "Hyperion/Rendering/Texture2D.h"
+
+/* Utilities */
+#include "Hyperion/Utilities/Input.h"
+#include "Hyperion/Utilities/Log.h"
+#include "Hyperion/Utilities/Random.h"
+#include "Hyperion/Utilities/Timestep.h"
