22f0f512e6bf7e41c3bc434353a1c38d2f52dbbe
SkillerRaptor <skillerraptor@protonmail.com>
2020-10-05 02:33:29 +0200

    Updating Components

--- HyperEngine/src/Hyperion/ECS/Components.hpp
+++ HyperEngine/src/Hyperion/ECS/Components.hpp
@@ -1,23 +1,49 @@
 #pragma once
 
+#include <string>
+
 #include "Maths/Vector.hpp"
 
 namespace Hyperion
 {
+	struct TagComponent
+	{
+		std::string Tag;
+
+		TagComponent() = default;
+		TagComponent(const TagComponent& tagComponent) = default;
+		TagComponent(const std::string& tag)
+			: Tag(tag) {}
+	};
+
 	struct TransformComponent
 	{
 		Vector3 Position;
 		Vector3 Rotation;
 		Vector3 Scale;
 
-		TransformComponent(Vector3 position = { 0.0f, 0.0f, 0.0f }, Vector3 rotation = { 0.0f, 0.0f, 0.0f }, Vector3 scale = { 0.0f, 0.0f, 0.0f })
+		TransformComponent() = default;
+		TransformComponent(const TransformComponent& transformComponent) = default;
+		TransformComponent(const Vector3& position, const Vector3& rotation, const Vector3& scale)
 			: Position(position), Rotation(rotation), Scale(scale) {}
+	};
+
+	struct SpriteRendererComponent
+	{
+		Vector4 Color;
+
+		SpriteRendererComponent() = default;
+		SpriteRendererComponent(const SpriteRendererComponent& spriteRendererComponent) = default;
+		SpriteRendererComponent(const Vector4& color)
+			: Color(color) {}
 	};
 
 	struct CharacterControllerComponent 
 	{
 		float Speed;
 
+		CharacterControllerComponent() = default;
+		CharacterControllerComponent(const CharacterControllerComponent& characterControllerComponent) = default;
 		CharacterControllerComponent(float speed)
 			: Speed(speed) {}
 	};
