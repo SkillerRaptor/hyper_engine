9d296a50a8be43a2a660bc4823a4252c9806a9af
SkillerRaptor <skillerraptor@protonmail.com>
2020-12-01 21:34:03 +0100

    [Rendering] Removing Material function

--- HyperEngine/src/Hyperion/HyperRendering/Material.cpp
+++ HyperEngine/src/Hyperion/HyperRendering/Material.cpp
@@ -5,79 +5,18 @@
 namespace Hyperion
 {
 	Material::Material(uint32_t shader)
-		: m_Shader(shader), m_Texture(0), m_Color(0.0f), m_Ambient(0.0f), m_Diffuse(0.0f), m_Specular(0.0f), m_Shininess(0.0f)
+		: m_Shader(shader)
 	{
+	}
+
+	Material::~Material()
+	{
+
 	}
 
 	void Material::Bind()
 	{
 		Ref<ShaderManager> shaderManager = Application::Get()->GetNativeWindow()->GetContext()->GetShaderManager();
-		shaderManager->UseShader(m_Shader);
-		shaderManager->SetInteger(m_Shader, "u_Material.Texture", m_Texture);
-		shaderManager->SetVector4(m_Shader, "u_Material.Color", m_Color);
-		shaderManager->SetVector3(m_Shader, "u_Material.Ambient", m_Ambient);
-		shaderManager->SetVector3(m_Shader, "u_Material.Diffuse", m_Diffuse);
-		shaderManager->SetVector3(m_Shader, "u_Material.Specular", m_Specular);
-		shaderManager->SetFloat(m_Shader, "u_Material.Shininess", m_Shininess);
-	}
 
-	void Material::SetColor(Vec4 color)
-	{
-		m_Color = color;
-	}
-
-	const Vec4& Material::GetColor() const
-	{
-		return m_Color;
-	}
-
-	void Material::SetTexture(uint32_t texture)
-	{
-		m_Texture = texture;
-	}
-
-	const uint32_t Material::GetTexture() const
-	{
-		return m_Texture;
-	}
-
-	void Material::SetAmbient(Vec3 ambient)
-	{
-		m_Ambient = ambient;
-	}
-
-	const Vec3& Material::GetAmbient() const
-	{
-		return m_Ambient;
-	}
-
-	void Material::SetDiffuse(Vec3 diffuse)
-	{
-		m_Diffuse = diffuse;
-	}
-
-	const Vec3& Material::GetDiffuse() const
-	{
-		return m_Diffuse;
-	}
-
-	void Material::SetSpecular(Vec3 specular)
-	{
-		m_Specular = specular;
-	}
-
-	const Vec3& Material::GetSpecular() const
-	{
-		return m_Specular;
-	}
-
-	void Material::SetShininess(float shininess)
-	{
-		m_Shininess = shininess;
-	}
-
-	const float Material::GetShininess() const
-	{
-		return m_Shininess;
 	}
 }
--- HyperEngine/src/Hyperion/HyperRendering/Material.hpp
+++ HyperEngine/src/Hyperion/HyperRendering/Material.hpp
@@ -8,34 +8,17 @@ namespace Hyperion
 	{
 	private:
 		uint32_t m_Shader;
-		uint32_t m_Texture;
-		Vec4 m_Color;
-		Vec3 m_Ambient;
-		Vec3 m_Diffuse;
-		Vec3 m_Specular;
-		float m_Shininess;
 
 	public:
 		Material(uint32_t shader);
+		~Material();
 
 		void Bind();
 
-		void SetColor(Vec4 color);
-		const Vec4& GetColor() const;
+		template<typename T>
+		void Set(const std::string& name, const T& value)
+		{
 
-		void SetTexture(uint32_t texture);
-		const uint32_t GetTexture() const;
-
-		void SetAmbient(Vec3 ambient);
-		const Vec3& GetAmbient() const;
-
-		void SetDiffuse(Vec3 diffuse);
-		const Vec3& GetDiffuse() const;
-
-		void SetSpecular(Vec3 specular);
-		const Vec3& GetSpecular() const;
-
-		void SetShininess(float shininess);
-		const float GetShininess() const;
+		}
 	};
 }
