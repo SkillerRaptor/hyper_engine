caeda9c6cf02ab1dfcf697b901852093e6ae1eab
SkillerRaptor <skillerraptor@protonmail.com>
2020-12-27 00:47:15 +0100

    [Core] Removing unesscary code & Adding filesystem macros

--- HyperEngine/src/Hyperion/HyperCore/Core.hpp
+++ HyperEngine/src/Hyperion/HyperCore/Core.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <filesystem>
 #include <memory>
 
 #if defined(_WIN32)
@@ -47,6 +46,47 @@
 	#define HP_DEBUGBREAK()
 #endif
 
+#ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
+	#if defined(__cpp_lib_filesystem)
+		#define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+	#elif defined(__cpp_lib_experimental_filesystem)
+		#define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+	#elif !defined(__has_include)
+		#define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+	#elif __has_include(<filesystem>)
+		#ifdef _MSC_VER
+			#if __has_include(<yvals_core.h>)
+					#include <yvals_core.h>
+				#if defined(_HAS_CXX17) && _HAS_CXX17
+					#define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+				#endif
+			#endif
+
+		#ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
+			#define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+		#endif
+
+			#define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+		#endif
+
+	#elif __has_include(<experimental/filesystem>)
+		#define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+
+	#else
+		#error Could not find system header "<filesystem>" or "<experimental/filesystem>"
+	#endif
+
+	#if INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
+		#include <experimental/filesystem>
+		namespace std
+		{
+			namespace filesystem = experimental::filesystem;
+		}
+	#else
+		#include <filesystem>
+	#endif
+#endif
+
 #define HP_EXPAND_MACRO(x) x
 #define HP_STRINGIFY_MACRO(x) #x
 
@@ -85,26 +125,4 @@ namespace Hyperion
 	#define HP_ASSERT(...) HP_EXPAND_MACRO( HP_INTERNAL_ASSERT_MACRO(__VA_ARGS__)(__VA_ARGS__) )
 #elif
 	#define HP_ASSERT(...)
-#endif
-
-#if false
-#ifdef HP_DEBUG
-#define HP_CORE_ASSERT(x, ...) \
-	{ \
-		if(!(x)) \
-		{ \
-			HP_CORE_ERROR("Assertion Failed: %", __VA_ARGS__); \
-			__debugbreak(); \
-		} \
-	}
-
-#define HP_CLIENT_ASSERT(x, ...) \
-	{ \
-		if (!(x)) \
-		{ \
-			HP_CLIENT_ERROR("Assertion Failed: %", __VA_ARGS__); \
-			__debugbreak(); \
-		} \
-	}
-#endif
 #endif
