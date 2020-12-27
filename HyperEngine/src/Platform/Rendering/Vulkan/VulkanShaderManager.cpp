0c3c0cd1916f37d5338d6d91ef333bacf298c1fe
SkillerRaptor <skillerraptor@protonmail.com>
2020-12-27 15:29:33 +0100

    [Rendering] Adding Vulkan shader manager back

---
+++ HyperEngine/src/Platform/Rendering/Vulkan/VulkanShaderManager.cpp
@@ -0,0 +1,14 @@
+#include "VulkanShaderManager.hpp"
+
+namespace Hyperion
+{
+	VulkanShaderManager::VulkanShaderManager()
+	{
+
+	}
+
+	VulkanShaderManager::~VulkanShaderManager()
+	{
+
+	}
+}
---
+++ HyperEngine/src/Platform/Rendering/Vulkan/VulkanShaderManager.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Hyperion
+{
+	class VulkanShaderManager
+	{
+	private:
+
+	public:
+		VulkanShaderManager();
+		~VulkanShaderManager();
+	};
+}
