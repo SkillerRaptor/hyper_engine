3f3b4cdb3e7930900f23c9c6b6b6303b77a00992
SkillerRaptor <skillerraptor@protonmail.com>
2021-01-12 01:11:22 +0100

    [Utilities] Updating logging to fmt

--- HyperEngine/src/Hyperion/HyperUtilities/Log.hpp
+++ HyperEngine/src/Hyperion/HyperUtilities/Log.hpp
@@ -20,14 +20,12 @@ namespace Hyperion
 }
 
 #define HP_CORE_INFO(...)    ::Hyperion::Log::GetCoreLogger()->PrintInfo(__VA_ARGS__)
-#define HP_CORE_TRACE(...)   ::Hyperion::Log::GetCoreLogger()->PrintTrace(__VA_ARGS__)
 #define HP_CORE_WARN(...)    ::Hyperion::Log::GetCoreLogger()->PrintWarn(__VA_ARGS__)
 #define HP_CORE_ERROR(...)   ::Hyperion::Log::GetCoreLogger()->PrintError(__VA_ARGS__)
 #define HP_CORE_FATAL(...)   ::Hyperion::Log::GetCoreLogger()->PrintFatal(__VA_ARGS__)
 #define HP_CORE_DEBUG(...)   ::Hyperion::Log::GetCoreLogger()->PrintDebug(__VA_ARGS__)
 
 #define HP_CLIENT_INFO(...)  ::Hyperion::Log::GetClientLogger()->PrintInfo(__VA_ARGS__)
-#define HP_CLIENT_TRACE(...) ::Hyperion::Log::GetClientLogger()->PrintTrace(__VA_ARGS__)
 #define HP_CLIENT_WARN(...)  ::Hyperion::Log::GetClientLogger()->PrintWarn(__VA_ARGS__)
 #define HP_CLIENT_ERROR(...) ::Hyperion::Log::GetClientLogger()->PrintError(__VA_ARGS__)
 #define HP_CLIENT_FATAL(...) ::Hyperion::Log::GetClientLogger()->PrintFatal(__VA_ARGS__)
