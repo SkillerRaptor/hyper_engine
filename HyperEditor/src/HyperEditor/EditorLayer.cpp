7bb19f2e9ee9d50012b487bd62dc306660edd1ee
SkillerRaptor <skillerraptor@protonmail.com>
2021-02-19 22:04:44 +0100

    [HyperEditor] Fixing ImGui errors

--- HyperEditor/src/HyperEditor/EditorLayer.cpp
+++ HyperEditor/src/HyperEditor/EditorLayer.cpp
@@ -86,8 +86,8 @@ namespace HyperEditor
 
 		ImGuiViewport* viewport = ImGui::GetMainViewport();
 		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoBackground;
-		ImGui::SetNextWindowPos(viewport->GetWorkPos());
-		ImGui::SetNextWindowSize(viewport->GetWorkSize());
+		ImGui::SetNextWindowPos(viewport->WorkPos);
+		ImGui::SetNextWindowSize(viewport->WorkSize);
 		ImGui::SetNextWindowViewport(viewport->ID);
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
