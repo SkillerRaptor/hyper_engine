000cc6c11cbbfd51904c116f4346d7be1e43a5b5
SkillerRaptor <skillerraptor@protonmail.com>
2021-02-26 01:16:34 +0100

    [General] Removing submodules and premake

--- HyperEngine/vendor/vulkan/src/vulkan.c
+++
@@ -1,1 +0,0 @@
-#include "../include/vulkan/vulkan.h"
--- Scripts/Linux-GenProjects.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=$(dirname "$SCRIPT")
-
-cd "$SCRIPT_PATH/.."
-chmod 777 ./vendor/premake/bin/premake5
-./vendor/premake/bin/premake5 gmake2
