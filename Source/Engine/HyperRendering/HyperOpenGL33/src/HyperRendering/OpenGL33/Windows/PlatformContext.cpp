7bbf8f689795cfb792535dc3a61149717dc8f596
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-19 19:15:09 +0200

    [HyperRendering] Adding specific opengl version

--- Source/Engine/HyperRendering/HyperOpenGL33/src/HyperRendering/OpenGL33/Windows/PlatformContext.cpp
+++ Source/Engine/HyperRendering/HyperOpenGL33/src/HyperRendering/OpenGL33/Windows/PlatformContext.cpp
@@ -7,7 +7,14 @@
 #include <HyperPlatform/PlatformDetection.hpp>
 
 #if HYPERENGINE_PLATFORM_WINDOWS
+#include <HyperCore/Logger.hpp>
 #include <HyperRendering/OpenGL33/Windows/PlatformContext.hpp>
+
+#define WGL_CONTEXT_MAJOR_VERSION_ARB 0x2091
+#define WGL_CONTEXT_MINOR_VERSION_ARB 0x2092
+#define WGL_CONTEXT_PROFILE_MASK_ARB 0x9126
+#define WGL_CONTEXT_CORE_PROFILE_BIT_ARB 0x00000001
+typedef HGLRC (WINAPI * PFNWGLCREATECONTEXTATTRIBSARBPROC)(HDC,HGLRC,const int*);
 
 namespace HyperRendering::OpenGL33::Windows
 {
@@ -15,40 +22,44 @@ namespace HyperRendering::OpenGL33::Windows
 	{
 		m_window = static_cast<HyperPlatform::Windows::CWindow*>(window);
 		
-		HDC window_handle = GetDC(m_window->handle());
-		
 		PIXELFORMATDESCRIPTOR pixel_format{};
 		pixel_format.nSize = sizeof(PIXELFORMATDESCRIPTOR);
 		pixel_format.nVersion = 1;
 		pixel_format.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
 		pixel_format.iPixelType = PFD_TYPE_RGBA;
 		pixel_format.cColorBits = 32;
-		pixel_format.cRedBits = 0;
-		pixel_format.cRedShift = 0;
-		pixel_format.cGreenBits = 0;
-		pixel_format.cGreenShift = 0;
-		pixel_format.cBlueBits = 0;
-		pixel_format.cBlueShift = 0;
-		pixel_format.cAlphaBits = 0;
-		pixel_format.cAlphaShift = 0;
-		pixel_format.cAccumBits = 0;
-		pixel_format.cAccumRedBits = 0;
-		pixel_format.cAccumGreenBits = 0;
-		pixel_format.cAccumBlueBits = 0;
-		pixel_format.cAccumAlphaBits = 0;
+		pixel_format.cRedBits = 8;
+		pixel_format.cGreenBits = 8;
+		pixel_format.cBlueBits = 8;
+		pixel_format.cAlphaBits = 8;
 		pixel_format.cDepthBits = 24;
 		pixel_format.cStencilBits = 8;
-		pixel_format.cAuxBuffers = 0;
 		pixel_format.iLayerType = PFD_MAIN_PLANE;
-		pixel_format.bReserved = 0;
-		pixel_format.dwLayerMask = 0;
-		pixel_format.dwVisibleMask = 0;
-		pixel_format.dwDamageMask = 0;
 		
-		SetPixelFormat(window_handle, ChoosePixelFormat(window_handle, &pixel_format), &pixel_format);
+		SetPixelFormat(m_window->handle(), ChoosePixelFormat(m_window->handle(), &pixel_format), &pixel_format);
 		
-		m_graphics_context = wglCreateContext(window_handle);
-		wglMakeCurrent(window_handle, m_graphics_context);
+		GLint context_attributes[] = {
+			WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+			WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+			WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+			0
+		};
+		m_graphics_context = wglCreateContext(m_window->handle());
+		wglMakeCurrent(m_window->handle(), m_graphics_context);
+		
+		PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB =
+			(PFNWGLCREATECONTEXTATTRIBSARBPROC) wglGetProcAddress("wglCreateContextAttribsARB");
+		
+		wglMakeCurrent(m_window->handle(), nullptr);
+		wglDeleteContext(m_graphics_context);
+		//m_graphics_context = wglCreateContext(m_window->handle());
+		m_graphics_context = wglCreateContextAttribsARB(m_window->handle(), nullptr, context_attributes);
+		wglMakeCurrent(m_window->handle(), m_graphics_context);
+		
+		HyperCore::CLogger::debug("OpenGL Info:");
+		HyperCore::CLogger::debug("  Vendor: {0}", glGetString(GL_VENDOR));
+		HyperCore::CLogger::debug("  Renderer: {0}", glGetString(GL_RENDERER));
+		HyperCore::CLogger::debug("  Version: {0}", glGetString(GL_VERSION));
 	}
 	
 	void CPlatformContext::shutdown()
@@ -58,7 +69,7 @@ namespace HyperRendering::OpenGL33::Windows
 	
 	void CPlatformContext::swap_buffers() const
 	{
-		wglSwapLayerBuffers(GetDC(m_window->handle()), WGL_SWAP_MAIN_PLANE);
+		SwapBuffers(m_window->handle());
 	}
 }
 #endif
