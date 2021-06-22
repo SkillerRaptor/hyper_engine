3f6fe0c54ffb6fdd23416da3eac9a9c756ac0e5c
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-22 21:14:18 +0200

    [HyperRendering] Destroying vulkan debug messenger

--- Source/Engine/HyperRendering/HyperVulkan/src/HyperRendering/Vulkan/Context.cpp
+++ Source/Engine/HyperRendering/HyperVulkan/src/HyperRendering/Vulkan/Context.cpp
@@ -31,11 +31,15 @@ namespace HyperRendering
 
 			if (severity_flags >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
 			{
-				HyperCore::CLogger::fatal("Vulkan Validation Error - {}", callback_data->pMessage);
+				HyperCore::CLogger::fatal(
+					"Vulkan Validation Error - {}", callback_data->pMessage);
 			}
-			else if (severity_flags >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+			else if (
+				severity_flags >=
+				VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
 			{
-				HyperCore::CLogger::warning("Vulkan Validation Warning - {}", callback_data->pMessage);
+				HyperCore::CLogger::warning(
+					"Vulkan Validation Warning - {}", callback_data->pMessage);
 			}
 
 			return VK_FALSE;
@@ -70,6 +74,20 @@ namespace HyperRendering
 
 		void CContext::shutdown()
 		{
+#if HYPERENGINE_DEBUG
+			if (m_validation_layer_support)
+			{
+				PFN_vkVoidFunction vkDestroyDebugUtilsMessengerEXTFunction =
+					vkGetInstanceProcAddr(
+						m_instance, "vkDestroyDebugUtilsMessengerEXT");
+				PFN_vkDestroyDebugUtilsMessengerEXT
+					vkDestroyDebugUtilsMessengerEXT =
+						reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(
+							vkDestroyDebugUtilsMessengerEXTFunction);
+				
+				vkDestroyDebugUtilsMessengerEXT(m_instance, m_debug_messenger, nullptr);
+			}
+#endif
 			vkDestroyInstance(m_instance, nullptr);
 		}
 
@@ -87,8 +105,9 @@ namespace HyperRendering
 			application_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 			application_info.apiVersion = VK_API_VERSION_1_2;
 
-			std::vector<const char*> extensions = { "VK_KHR_surface",
-													m_platform_context->get_required_extension() };
+			std::vector<const char*> extensions = {
+				"VK_KHR_surface", m_platform_context->get_required_extension()
+			};
 			if (m_validation_layer_support)
 			{
 				extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
@@ -114,19 +133,22 @@ namespace HyperRendering
 					VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
 				debug_messenger_create_info.pfnUserCallback = debug_callback;
 
-				next = reinterpret_cast<const void*>(&debug_messenger_create_info);
+				next =
+					reinterpret_cast<const void*>(&debug_messenger_create_info);
 			}
 
 			VkInstanceCreateInfo instance_create_info{};
 			instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 			instance_create_info.pApplicationInfo = &application_info;
-			instance_create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+			instance_create_info.enabledExtensionCount =
+				static_cast<uint32_t>(extensions.size());
 			instance_create_info.ppEnabledExtensionNames = extensions.data();
 			instance_create_info.enabledLayerCount = layer_count;
 			instance_create_info.ppEnabledLayerNames = layers;
 			instance_create_info.pNext = next;
 
-			if (vkCreateInstance(&instance_create_info, nullptr, &m_instance) != VK_SUCCESS)
+			if (vkCreateInstance(&instance_create_info, nullptr, &m_instance) !=
+				VK_SUCCESS)
 			{
 				HyperCore::CLogger::fatal("Failed to create vulkan instance!");
 				return false;
@@ -143,24 +165,31 @@ namespace HyperRendering
 			}
 
 			VkDebugUtilsMessengerCreateInfoEXT debug_messenger_create_info{};
-			debug_messenger_create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+			debug_messenger_create_info.sType =
+				VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
 			debug_messenger_create_info.messageSeverity =
 				VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
 				VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-			debug_messenger_create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
-													  VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+			debug_messenger_create_info.messageType =
+				VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+				VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
 			debug_messenger_create_info.pfnUserCallback = debug_callback;
 
 			PFN_vkVoidFunction vkCreateDebugUtilsMessengerEXTFunction =
-				vkGetInstanceProcAddr(m_instance, "vkCreateDebugUtilsMessengerEXT");
+				vkGetInstanceProcAddr(
+					m_instance, "vkCreateDebugUtilsMessengerEXT");
 			PFN_vkCreateDebugUtilsMessengerEXT vkCreateDebugUtilsMessengerEXT =
 				reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(
 					vkCreateDebugUtilsMessengerEXTFunction);
 
 			if (vkCreateDebugUtilsMessengerEXT(
-					m_instance, &debug_messenger_create_info, nullptr, &m_debug_messenger) != VK_SUCCESS)
+					m_instance,
+					&debug_messenger_create_info,
+					nullptr,
+					&m_debug_messenger) != VK_SUCCESS)
 			{
-				HyperCore::CLogger::fatal("Failed to setup vulkan debug messenger!");
+				HyperCore::CLogger::fatal(
+					"Failed to setup vulkan debug messenger!");
 				return false;
 			}
 
@@ -170,19 +199,24 @@ namespace HyperRendering
 		bool CContext::is_validation_layer_available()
 		{
 			uint32_t available_layer_properties_count = 0;
-			vkEnumerateInstanceLayerProperties(&available_layer_properties_count, nullptr);
-
-			std::vector<VkLayerProperties> available_layer_properties(available_layer_properties_count);
 			vkEnumerateInstanceLayerProperties(
-				&available_layer_properties_count, available_layer_properties.data());
+				&available_layer_properties_count, nullptr);
+
+			std::vector<VkLayerProperties> available_layer_properties(
+				available_layer_properties_count);
+			vkEnumerateInstanceLayerProperties(
+				&available_layer_properties_count,
+				available_layer_properties.data());
 
 			for (const char* layer_name : s_validation_layers)
 			{
 				bool layer_found = false;
 
-				for (const VkLayerProperties& layer_properties : available_layer_properties)
+				for (const VkLayerProperties& layer_properties :
+					 available_layer_properties)
 				{
-					if (std::strcmp(layer_name, layer_properties.layerName) != 0)
+					if (std::strcmp(layer_name, layer_properties.layerName) !=
+						0)
 					{
 						continue;
 					}
