523866656939cb00fd27dfddc5328dbd885a86da
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-26 22:31:33 +0200

    [HyperVulkan] Adding cstring header

--- Source/Engine/HyperRendering/HyperVulkan/src/HyperRendering/Vulkan/Context.cpp
+++ Source/Engine/HyperRendering/HyperVulkan/src/HyperRendering/Vulkan/Context.cpp
@@ -9,6 +9,7 @@
 #include <HyperPlatform/IWindow.hpp>
 #include <HyperRendering/Vulkan/Context.hpp>
 #include <HyperRendering/Vulkan/IPlatformContext.hpp>
+#include <cstring>
 #include <string>
 #include <vulkan/vulkan.h>
 
