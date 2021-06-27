97a508b34a27e33c7981c4dba2747ae090f8f226
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-28 01:39:50 +0200

    [HyperMath] Adding missing header

--- Source/Engine/HyperMath/src/HyperMath/HyperMath.cpp
+++ Source/Engine/HyperMath/src/HyperMath/HyperMath.cpp
@@ -3,3 +3,11 @@
  *
  * SPDX-License-Identifier: MIT
  */
+
+#include <HyperMath/Matrix2x2.hpp>
+#include <HyperMath/Matrix3x3.hpp>
+#include <HyperMath/Matrix4x4.hpp>
+#include <HyperMath/Quaternion.hpp>
+#include <HyperMath/Vector2.hpp>
+#include <HyperMath/Vector3.hpp>
+#include <HyperMath/Vector4.hpp>
