99913bfdf8a7ed7e9bd39db2f7c905709d8a81dd
SkillerRaptor <skillerraptor@protonmail.com>
2021-07-10 11:20:00 +0200

    [General] Updating headers
    Changing year in copyright header from 2021 to 2020-2021

--- Source/Engine/HyperPlatform/src/HyperPlatform/IWindow.hpp
+++ Source/Engine/HyperPlatform/src/HyperPlatform/IWindow.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, SkillerRaptor <skillerraptor@protonmail.com>
+ * Copyright (c) 2020-2021, SkillerRaptor <skillerraptor@protonmail.com>
  *
  * SPDX-License-Identifier: MIT
  */
