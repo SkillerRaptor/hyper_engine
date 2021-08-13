09715c342842fcf9c602bdcc2329bce6387175af
SkillerRaptor <skillerraptor@protonmail.com>
2021-08-13 14:07:30 +0200

    [HyperMath] Adding lerp & midpoint as utilities function

--- Source/Engine/HyperMath/include/HyperMath/Common.hpp
+++ Source/Engine/HyperMath/include/HyperMath/Common.hpp
@@ -6,14 +6,15 @@
 
 #pragma once
 
+#include <limits>
 #include <type_traits>
 
 #ifdef min
-	#undef min
+#	undef min
 #endif
 
 #ifdef max
-	#undef max
+#	undef max
 #endif
 
 namespace HyperMath
@@ -23,16 +24,62 @@ namespace HyperMath
 	{
 		return x >= 0 ? x : -x;
 	}
-	
+
 	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
 	static constexpr auto min(T a, T b) -> T
 	{
 		return b < a ? b : a;
 	}
-	
+
 	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
 	static constexpr auto max(T a, T b) -> T
 	{
 		return a < b ? b : a;
 	}
-}
+
+	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+	static constexpr auto midpoint(T a, T b) -> T
+	{
+		if constexpr (std::is_integral_v<T>)
+		{
+			using U = std::make_unsigned_t<T>;
+
+			int sign = 1;
+			
+			U first = a;
+			U second = b;
+			if (a > b)
+			{
+				sign = -1;
+				first = b;
+				second = a;
+			}
+
+			return a + sign * static_cast<T>(static_cast<U>(second - first) / 2);
+		}
+		else
+		{
+			constexpr T low = std::numeric_limits<T>::min() * 2;
+			constexpr T high = std::numeric_limits<T>::max() * 2;
+			const T absolute_a = HyperMath::abs<T>(a);
+			const T absolute_b = HyperMath::abs<T>(b);
+			
+			if (absolute_a <= high && absolute_b <= high)
+			{
+				return (a + b) / 2;
+			}
+			
+			if (absolute_a < low)
+			{
+				return a + b / 2;
+			}
+			
+			if (absolute_b < low)
+			{
+				return a / 2 + b;
+			}
+			
+			return a / 2 + b / 2;
+		}
+	}
+} // namespace HyperMath
--- Source/Engine/HyperMath/include/HyperMath/Trigonometric.hpp
+++ Source/Engine/HyperMath/include/HyperMath/Trigonometric.hpp
@@ -17,10 +17,27 @@ namespace HyperMath
 	{
 		return degrees * (Numbers::pi_v<T> / static_cast<T>(180.0L));
 	}
-	
+
 	template <typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
 	static constexpr auto degrees(T&& radians) -> T
 	{
 		return radians * (static_cast<T>(180.0L) / Numbers::pi_v<T>);
 	}
-}
+
+	template <typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
+	static constexpr auto lerp(T a, T b, T interpolation) -> T
+	{
+		if ((a <= 0 && b >= 0) || (a >= 0 && b <= 0))
+		{
+			return interpolation * b + (1 - interpolation) * a;
+		}
+
+		if (interpolation == 1)
+		{
+			return b;
+		}
+
+		const T x = a + interpolation * (b - a);
+		return (interpolation > 1) == (b > a) ? (b < x ? x : b) : (b > x ? x : b);
+	}
+} // namespace HyperMath
