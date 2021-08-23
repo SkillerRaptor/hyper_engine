fa9bb9d6037060a8d8fb3fe12ea06e5f783dbd65
SkillerRaptor <skillerraptor@protonmail.com>
2021-08-23 19:26:52 +0200

    [HyperVulkan] Starting with graphics pipeline

---
+++ Source/Engine/HyperRendering/HyperVulkan/src/HyperVulkan/GraphicsPipeline.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020-2021, SkillerRaptor <skillerraptor@protonmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "HyperVulkan/GraphicsPipeline.hpp"
+
+#include "HyperVulkan/Context.hpp"
+
+namespace HyperRendering::Vulkan
+{
+	GraphicsPipeline::GraphicsPipeline(Context& context)
+		: m_context(context)
+	{
+	}
+
+	auto GraphicsPipeline::initialize() -> HyperCore::Result<void, HyperCore::Errors::ConstructError>
+	{
+		return {};
+	}
+
+	auto GraphicsPipeline::terminate() -> void
+	{
+	}
+} // namespace HyperRendering::Vulkan
