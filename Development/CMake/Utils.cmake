8a7020854580e34d8d4ae52b1f802a8bfa8ed4d6
SkillerRaptor <skillerraptor@protonmail.com>
2021-08-28 13:46:03 +0200

    General: Refactoring

--- Development/CMake/Utils.cmake
+++ Development/CMake/Utils.cmake
@@ -7,29 +7,29 @@
 #-------------------------------------------------------------------------------------------
 # Utils
 #-------------------------------------------------------------------------------------------
-macro(hyperengine_group_files files)
+function(hyperengine_group_files files)
     foreach (item IN ITEMS ${files})
         get_filename_component(src_path "${item}" PATH)
         string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}" "" group_path "${src_path}")
         string(REPLACE ".." "\\" group_path "${group_path}")
         source_group("${group_path}" FILES "${item}")
     endforeach ()
-endmacro()
+endfunction()
 
-macro(hyperengine_define_executable target)
+function(hyperengine_define_executable target)
     add_executable(${target} ${SOURCES} ${HEADERS})
     target_link_libraries(${target} PRIVATE ProjectOptions ProjectWarnings)
     target_include_directories(${target} PUBLIC include)
-endmacro()
+endfunction()
 
-macro(hyperengine_define_library target)
+function(hyperengine_define_library target)
     add_library(${target} STATIC ${SOURCES} ${HEADERS})
     target_compile_features(${target} PUBLIC cxx_std_17)
     target_link_libraries(${target} PRIVATE ProjectOptions ProjectWarnings)
     target_include_directories(${target} PUBLIC include)
-endmacro()
+endfunction()
 
-macro(hyperengine_define_module target)
+function(hyperengine_define_module target)
     hyperengine_define_library(${target})
     set_target_properties(${target} PROPERTIES FOLDER HyperModules)
-endmacro()
+endfunction()
