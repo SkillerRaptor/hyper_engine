486d0f1e95d39963b450039c2c58a9daaefb6d48
SkillerRaptor <skillerraptor@protonmail.com>
2021-08-29 22:35:55 +0200

    General: Removing not used dependencies

--- Source/Engine/HyperResource/src/HyperResource/HyperResource.cpp
+++ Source/Engine/HyperResource/src/HyperResource/HyperResource.cpp
@@ -3,11 +3,3 @@
  *
  * SPDX-License-Identifier: MIT
  */
-
-#include <glslang/Include/Common.h>
-#include <meshoptimizer.h>
-#include <spirv_cross.hpp>
-#include <spirv-tools/libspirv.hpp>
-#include <stb_image.h>
-#include <tiny_gltf.h>
-#include <tiny_obj_loader.h>
