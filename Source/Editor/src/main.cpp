83a95f73b7e69b4058a8423b412f44f0ec14a620
SkillerRaptor <skillerraptor@protonmail.com>
2021-10-01 09:47:49 +0200

    Editor: Redirecting stdout buffer

--- Source/Editor/src/main.cpp
+++ Source/Editor/src/main.cpp
@@ -8,7 +8,10 @@
 
 #include <HyperEngine/Launcher.hpp>
 
+#include <cstdio>
+
 auto main() -> int
 {
+	std::setvbuf(stdout, nullptr, _IONBF, 0);
 	return HyperEngine::CLauncher::launch<HyperEditor::Editor>();
 }
