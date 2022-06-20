960d48c72756f685e2a0adbf95caa9875a11c1e4
SkillerRaptor <skillerraptor@protonmail.com>
2022-06-20 10:17:13 +0200

    Added entry point

---
+++ engine/src/hyper_engine/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022-present, SkillerRaptor <skillerraptor@protonmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World!\n");
+	return 0;
+}
