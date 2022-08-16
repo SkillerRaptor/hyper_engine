b78ce85dc251df2d3a5166c9226cdf439f45e4a4
SkillerRaptor <skillerraptor@protonmail.com>
2022-08-16 19:51:08 +0200

    Changed log format

--- src/core/window.rs
+++ src/core/window.rs
@@ -42,7 +42,7 @@ impl Window {
             .build(&event_loop)?;
 
         info!(
-            "Created window '{}' (width={}, height={})",
+            "Created window '{}' ({}x{})",
             title,
             native_window.inner_size().width,
             native_window.inner_size().height
