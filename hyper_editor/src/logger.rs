07cc86696b7ca4114b219655779f46057d014c9f
SkillerRaptor <skillerraptor@protonmail.com>
2022-11-05 16:53:13 +0100

    Editor: Seperated logger
    * Seperated logger from main.rs to logger.rs
    
    * Changed logger messages by adding date
    
    * Added possibility to add CLI to change verbosity of the logger

---
+++ hyper_editor/src/logger.rs
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022, SkillerRaptor <skillerraptor@protonmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+use chrono::Local;
+use color_eyre::Result;
+use fern::{
+    colors::{Color, ColoredLevelConfig},
+    Dispatch,
+};
+use log::LevelFilter;
+use std::io;
+
+pub fn init(verbosity: usize) -> Result<()> {
+    let levels = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::BrightYellow)
+        .info(Color::Green)
+        .debug(Color::Cyan)
+        .trace(Color::Magenta);
+
+    let mut logger = Dispatch::new();
+
+    logger = logger.format(move |out, message, record| {
+        let reset = "\x1B[0m";
+
+        let time = {
+            let current_time = Local::now().format("%d-%m-%Y %H:%M:%S");
+            let black = format!("\x1B[{}m", Color::BrightBlack.to_fg_str());
+            format!("{black}{current_time}{reset}")
+        };
+
+        let level = {
+            let current_level = record.level().as_str().to_lowercase();
+            let color = format!("\x1B[{}m", levels.get_color(&record.level()).to_fg_str());
+            format!("{color}{current_level}{reset}")
+        };
+
+        let message = format!("{reset}{message}");
+
+        out.finish(format_args!("{time} | {level}: {message}"))
+    });
+
+    logger = match verbosity {
+        0 => logger.level(LevelFilter::Warn),
+        1 => logger.level(LevelFilter::Info),
+        2 => logger.level(LevelFilter::Debug),
+        3 => logger.level(LevelFilter::Trace),
+        _ => logger.level(LevelFilter::Error),
+    };
+
+    logger = logger.chain(io::stdout());
+
+    logger.apply()?;
+
+    Ok(())
+}
--- hyper_editor/src/main.rs
+++ hyper_editor/src/main.rs
@@ -4,86 +4,18 @@
  * SPDX-License-Identifier: MIT
  */
 
+mod logger;
+
 use hyper_engine::application::Application;
 
-use chrono::Local;
 use color_eyre::Result;
-use colored::Colorize;
-use fern::Dispatch;
-use log::{Level, LevelFilter};
-use std::{fs, io};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-
-    install_logger()?;
+    logger::init(3)?; // Using hardcoded value until cli parsing is added
 
     let mut application = Application::new()?;
     application.run()?;
-
-    Ok(())
-}
-
-fn install_logger() -> Result<()> {
-    const LOGS_FOLDER: &str = "./logs";
-
-    fs::create_dir_all(LOGS_FOLDER)?;
-
-    let log_level = if cfg!(debug_assertions) {
-        LevelFilter::Debug
-    } else {
-        LevelFilter::Info
-    };
-
-    let latest_log_file = format!("{}/latest.log", LOGS_FOLDER);
-    let current_log_file = format!(
-        "{}/hyper_engine_{}.log",
-        LOGS_FOLDER,
-        Local::now().format("%d-%m-%Y_%H-%M-%S")
-    );
-
-    Dispatch::new()
-        .level(log_level)
-        .chain(
-            Dispatch::new()
-                .format(|out, message, record| {
-                    let time = Local::now().format("%H:%M:%S");
-                    let level = match record.level() {
-                        Level::Error => "error".red(),
-                        Level::Warn => "warn".bright_yellow(),
-                        Level::Info => "info".green(),
-                        Level::Debug => "debug".cyan(),
-                        Level::Trace => "trace".purple(),
-                    };
-
-                    out.finish(format_args!("{} | {}: {}", time, level, message))
-                })
-                .chain(io::stdout()),
-        )
-        .chain(
-            Dispatch::new()
-                .format(|out, message, record| {
-                    let time = Local::now().format("%H:%M:%S");
-                    let level = match record.level() {
-                        Level::Error => "error",
-                        Level::Warn => "warn",
-                        Level::Info => "info",
-                        Level::Debug => "debug",
-                        Level::Trace => "trace",
-                    };
-
-                    out.finish(format_args!("{} | {}: {}", time, level, message))
-                })
-                .chain(
-                    std::fs::OpenOptions::new()
-                        .write(true)
-                        .create(true)
-                        .truncate(true)
-                        .open(&latest_log_file)?,
-                )
-                .chain(fern::log_file(&current_log_file)?),
-        )
-        .apply()?;
 
     Ok(())
 }
