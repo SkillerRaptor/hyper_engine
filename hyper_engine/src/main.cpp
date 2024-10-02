04bb7727afa76cd195be904fb515fd89d5387114
SkillerRaptor <skillerraptor@protonmail.com>
2024-10-02 06:15:35 +0200

    feat: add argument parser

--- hyper_engine/src/main.cpp
+++ hyper_engine/src/main.cpp
@@ -4,13 +4,44 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include "hyper_platform/window.hpp"
+#include <exception>
 
-int main()
+#include <argparse/argparse.hpp>
+
+#include <hyper_core/logger.hpp>
+#include <hyper_platform/window.hpp>
+
+int main(int argc, char** argv)
 {
+    argparse::ArgumentParser program("HyperEngine");
+
+    uint32_t width = 0;
+    program.add_argument("--width").default_value(static_cast<uint32_t>(1280)).scan<'i', uint32_t>().store_into(width);
+
+    uint32_t height = 0;
+    program.add_argument("--height").default_value(static_cast<uint32_t>(720)).scan<'i', uint32_t>().store_into(height);
+
+    std::string renderer = "vulkan";
+    program.add_argument("--renderer").default_value("vulkan").choices("d3d12", "vulkan").store_into(renderer);
+
+    bool debug = false;
+    program.add_argument("--debug").default_value(false).implicit_value(true).store_into(debug);
+
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::exception& error)
+    {
+        HE_ERROR("{}", error.what());
+        return 1;
+    }
+
     hyper_platform::Window window = hyper_platform::Window({
         .title = "HyperEngine",
-        .width = 1280,
-        .height = 720,
+        .width = width,
+        .height = height,
     });
+
+    return 0;
 }
