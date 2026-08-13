a9808db0f1491838f87b7f100cced3d37e924a3b
SkillerRaptor <skillerraptor@protonmail.com>
2026-08-13 19:48:08 +0200

    feat: changed duration

--- hyper_engine/src/engine.cpp
+++ hyper_engine/src/engine.cpp
@@ -44,10 +44,10 @@ void Engine::run()
     std::chrono::time_point<std::chrono::steady_clock> current_time = std::chrono::steady_clock::now();
     while (!m_window->is_close_requested()) {
         const std::chrono::time_point<std::chrono::steady_clock> new_time = std::chrono::steady_clock::now();
-        const f32 frame_time = (new_time - current_time).count();
+        const std::chrono::duration<f32> frame_time = new_time - current_time;
         current_time = new_time;
 
-        accumulator += frame_time;
+        accumulator += frame_time.count();
 
         m_window->update();
         m_input.update();
